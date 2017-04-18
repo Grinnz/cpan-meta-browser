f00553fab96ed8a0b08c63f2d06189f3481b38aa
Dan Book <grinnz@grinnz.com>
2017-04-18 18:23:20 -0400

    add stickytabs plugin

--- cpan-meta-browser.pl
+++ cpan-meta-browser.pl
@@ -318,8 +318,10 @@ __DATA__
 
   <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="/js/jquery.stickytabs.js"></script>
   <script type="text/javascript">
     $(function() {
+      $('.nav-tabs').stickyTabs();
       $('#package-search-form').submit(function(event) {
         event.preventDefault();
         var package_name = $('#package-search-text-input').val();
