57d33cb868559e7b75e782dd94ff776091a85daa
Dan Book <grinnz@grinnz.com>
2017-08-16 18:00:29 -0400

    use vue.js

--- public/js/cpan-meta-browser.js
+++ public/js/cpan-meta-browser.js
@@ -3,215 +3,92 @@
      The Artistic License 2.0 (GPL Compatible)
 */
 
-$(function() {
-  $('#package-search-form').submit(function(event) {
-    event.preventDefault();
-    var package_name = $('#package-search-text-input').val();
-    var exact_match = $('#package-search-exact-match').is(':checked');
-    if (package_name.length === 0 || (!exact_match && package_name.length === 1)) {
-      return null;
-    }
-    $('#package-search-results-header').nextAll('tr').remove();
-    var res = $.getJSON('/api/v1/packages/' + encodeURIComponent(package_name), { as_prefix: exact_match ? 0 : 1 })
-      .done(function(data) {
-        $('#package-search-results-header').nextAll('tr').remove();
-        data.forEach(function(row_result) {
-          var new_row = $('<tr></tr>');
-          ['module','version','owner','uploader','path'].forEach(function(key) {
-            var cell = $('<td></td>');
-            var value = row_result[key];
-            switch (key) {
-              case 'module':
-                var url = 'https://metacpan.org/pod/' + encodeURI(value);
-                cell.append($('<a></a>').attr('href', url).text(value));
-                break;
-              case 'owner':
-              case 'uploader':
-                var url = 'https://metacpan.org/author/' + encodeURIComponent(value);
-                cell.append($('<a></a>').attr('href', url).text(value));
-                break;
-              case 'path':
-                var url = 'https://cpan.metacpan.org/authors/id/' + encodeURI(value);
-                cell.append($('<a></a>').attr('href', url).text(value));
-                break;
-              default:
-                cell.text(value);
-            }
-            new_row.append(cell);
-          });
-          $('#package-search-results-table').append(new_row);
+var search_data = { packages: [], module_perms: [], author_perms: [], authors: [] };
+var search_vm = new Vue({
+  el: '#search-tab',
+  data: search_data,
+  methods: {
+    search_packages: function() {
+      var package_name = $('#package-search-text-input').val();
+      var exact_match = $('#package-search-exact-match').is(':checked');
+      if (package_name.length === 0 || (!exact_match && package_name.length === 1)) {
+        return null;
+      }
+      var res = $.getJSON('/api/v1/packages/' + encodeURIComponent(package_name), { as_prefix: exact_match ? 0 : 1 })
+        .done(function(data) {
+          search_data.packages = data;
+        })
+        .fail(function() {
         });
-      })
-      .fail(function() {
-      });
-  });
-  $('#module-perms-search-form').submit(function(event) {
-    event.preventDefault();
-    var package_name = $('#module-perms-search-text-input').val();
-    var exact_match = $('#module-perms-search-exact-match').is(':checked');
-    if (package_name.length === 0 || (!exact_match && package_name.length === 1)) {
-      return null;
-    }
-    $('#module-perms-search-results-header').nextAll('tr').remove();
-    var res = $.getJSON('/api/v1/perms/by-module/' + encodeURIComponent(package_name), { as_prefix: exact_match ? 0 : 1 })
-      .done(function(data) {
-        $('#module-perms-search-results-header').nextAll('tr').remove();
-        data.forEach(function(row_result) {
-          var new_row = $('<tr></tr>');
-          ['module','author','best_permission','owner'].forEach(function(key) {
-            var cell = $('<td></td>');
-            var value = row_result[key];
-            switch (key) {
-              case 'module':
-                var url = 'https://metacpan.org/pod/' + encodeURI(value);
-                cell.append($('<a></a>').attr('href', url).text(value));
-                break;
-              case 'author':
-              case 'owner':
-                var url = 'https://metacpan.org/author/' + encodeURIComponent(value);
-                cell.append($('<a></a>').attr('href', url).text(value));
-                break;
-              case 'best_permission':
-                var perm = '';
-                switch (row_result[key].toLowerCase()) {
-                  case 'm':
-                    perm = 'modulelist';
-                    break;
-                  case 'f':
-                    perm = 'first-come';
-                    break;
-                  case 'c':
-                    perm = 'co-maint';
-                    break;
-                }
-                cell.text(perm);
-                break;
-              default:
-                cell.text(value);
-            }
-            new_row.append(cell);
-          });
-          $('#module-perms-search-results-table').append(new_row);
+    },
+    search_module_perms: function() {
+      var package_name = $('#module-perms-search-text-input').val();
+      var exact_match = $('#module-perms-search-exact-match').is(':checked');
+      if (package_name.length === 0 || (!exact_match && package_name.length === 1)) {
+        return null;
+      }
+      var res = $.getJSON('/api/v1/perms/by-module/' + encodeURIComponent(package_name), { as_prefix: exact_match ? 0 : 1 })
+        .done(function(data) {
+          search_data.module_perms = data;
+        })
+        .fail(function() {
         });
-      })
-      .fail(function() {
-      });
-  });
-  $('#author-perms-search-form').submit(function(event) {
-    event.preventDefault();
-    var author_id = $('#author-perms-search-text-input').val();
-    if (author_id.length === 0) {
-      return null;
+    },
+    search_author_perms: function() {
+      var author_id = $('#author-perms-search-text-input').val();
+      if (author_id.length === 0) {
+        return null;
+      }
+      var res = $.getJSON('/api/v1/perms/by-author/' + encodeURIComponent(author_id))
+        .done(function(data) {
+          search_data.author_perms = data;
+        })
+        .fail(function() {
+        })
+    },
+    search_authors: function() {
+      var author_id = $('#author-search-text-input').val();
+      var exact_match = $('#author-search-exact-match').is(':checked');
+      if (author_id.length === 0 || (!exact_match && author_id.length === 1)) {
+        return null;
+      }
+      var res = $.getJSON('/api/v1/authors/' + encodeURIComponent(author_id), { as_prefix: exact_match ? 0 : 1 })
+        .done(function(data) {
+          search_data.authors = data;
+        })
+        .fail(function() {
+        })
+    },
+    module_url: function(module) {
+      return 'https://metacpan.org/pod/' + encodeURI(module);
+    },
+    author_url: function(author) {
+      return 'https://metacpan.org/author/' + encodeURIComponent(author);
+    },
+    cpandir_url: function(path) {
+      return 'https://cpan.metacpan.org/authors/id/' + encodeURI(path);
+    },
+    permission_string: function(perm) {
+      switch (perm.toLowerCase()) {
+        case 'm':
+          return 'modulelist';
+        case 'f':
+          return 'first-come';
+        case 'c':
+          return 'co-maint';
+        default:
+          return '';
+      }
+    },
+    to_date_string: function(epoch) {
+      if (epoch != null) {
+        var date = new Date(epoch * 1000);
+        return date.toUTCString();
+      }
+      return '';
+    },
+    author_cpandir: function(author) {
+      return author.substring(0, 1) + '/' + author.substring(0, 2) + '/' + author;
     }
-    $('#author-perms-search-results-header').nextAll('tr').remove();
-    var res = $.getJSON('/api/v1/perms/by-author/' + encodeURIComponent(author_id))
-      .done(function(data) {
-        $('#author-perms-search-results-header').nextAll('tr').remove();
-        data.forEach(function(row_result) {
-          var new_row = $('<tr></tr>');
-          ['module','author','best_permission','owner'].forEach(function(key) {
-            var cell = $('<td></td>');
-            var value = row_result[key];
-            switch (key) {
-              case 'module':
-                var url = 'https://metacpan.org/pod/' + encodeURI(value);
-                cell.append($('<a></a>').attr('href', url).text(value));
-                break;
-              case 'author':
-              case 'owner':
-                var url = 'https://metacpan.org/author/' + encodeURIComponent(value);
-                cell.append($('<a></a>').attr('href', url).text(value));
-                break;
-              case 'best_permission':
-                var perm = '';
-                switch (row_result[key].toLowerCase()) {
-                  case 'm':
-                    perm = 'modulelist';
-                    break;
-                  case 'f':
-                    perm = 'first-come';
-                    break;
-                  case 'c':
-                    perm = 'co-maint';
-                    break;
-                }
-                cell.text(perm);
-                break;
-              default:
-                cell.text(value);
-            }
-            new_row.append(cell);
-          });
-          $('#author-perms-search-results-table').append(new_row);
-        });
-      })
-      .fail(function() {
-      });
-  });
-  $('#author-search-form').submit(function(event) {
-    event.preventDefault();
-    var author_id = $('#author-search-text-input').val();
-    var exact_match = $('#author-search-exact-match').is(':checked');
-    if (author_id.length === 0 || (!exact_match && author_id.length === 1)) {
-      return null;
-    }
-    $('#author-search-results-header').nextAll('tr').remove();
-    var res = $.getJSON('/api/v1/authors/' + encodeURIComponent(author_id), { as_prefix: exact_match ? 0 : 1 })
-      .done(function(data) {
-        $('#author-search-results-header').nextAll('tr').remove();
-        data.forEach(function(row_result) {
-          var new_row = $('<tr></tr>');
-          ['author','fullname','email','homepage','introduced','has_cpandir'].forEach(function(key) {
-            var cell = $('<td></td>');
-            var value = row_result[key];
-            switch (key) {
-              case 'author':
-                var url = 'https://metacpan.org/author/' + encodeURIComponent(value);
-                cell.append($('<a></a>').attr('href', url).text(value));
-                break;
-              case 'fullname':
-                cell.text(value);
-                var asciiname = row_result.asciiname;
-                if (asciiname != null) {
-                  cell.attr('title', asciiname);
-                }
-                break;
-              case 'email':
-                if (value != null && value === 'CENSORED') {
-                  cell.text(value);
-                } else {
-                  var url = 'mailto:' + value;
-                  cell.append($('<a></a>').attr('href', url).text(value));
-                }
-                break;
-              case 'homepage':
-                cell.append($('<a></a>').attr('href', value).text(value));
-                break;
-              case 'introduced':
-                if (value != null) {
-                  var introduced_date = new Date(value * 1000);
-                  cell.text(introduced_date.toUTCString());
-                }
-                break;
-              case 'has_cpandir':
-                if (value) {
-                  var cpanid = row_result.author;
-                  var first_dir = cpanid.substring(0, 1);
-                  var second_dir = cpanid.substring(0, 2);
-                  var cpandir = '/authors/id/' + first_dir + '/' + second_dir + '/' + cpanid;
-                  var url = 'https://cpan.metacpan.org' + encodeURI(cpandir);
-                  cell.append($('<a></a>').attr('href', url).text(cpandir));
-                }
-                break;
-              default:
-                cell.text(value);
-            }
-            new_row.append(cell);
-          });
-          $('#author-search-results-table').append(new_row);
-        });
-      })
-      .fail(function() {
-      });
-  });
+  }
 });
