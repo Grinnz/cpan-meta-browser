3c0d52eeb08730aeb142eb4493e19de2d642a666
Dan Book <grinnz@grinnz.com>
2018-02-08 18:08:52 -0500

    js cleanup?

--- public/js/cpan-meta-browser.js
+++ public/js/cpan-meta-browser.js
@@ -40,18 +40,18 @@ var search_vm = new Vue({
       if (query.length === 0 || (as_prefix && query.length === 1)) {
         search_data.package_search_results = null;
         search_data.package_data_refreshed = null;
-        return null;
+      } else {
+        var packages_url = new URL('/api/v2/packages/' + encodeURIComponent(query), window.location.href);
+        packages_url.searchParams.set('as_prefix', as_prefix ? 1 : 0);
+        fetch(packages_url).then(function(response) {
+          return response.json();
+        }).then(function(data) {
+          search_data.package_search_results = data.data;
+          search_data.package_data_refreshed = data.last_updated;
+        }).catch(function(error) {
+          console.log('Error retrieving packages', error);
+        });
       }
-      var packages_url = new URL('/api/v2/packages/' + encodeURIComponent(query), window.location.href);
-      packages_url.searchParams.set('as_prefix', as_prefix ? 1 : 0);
-      fetch(packages_url).then(function(response) {
-        return response.json();
-      }).then(function(data) {
-        search_data.package_search_results = data.data;
-        search_data.package_data_refreshed = data.last_updated;
-      }).catch(function(error) {
-        console.log('Error retrieving packages', error);
-      });
     },
     search_perms: function() {
       var query = search_data.search_query;
@@ -61,20 +61,20 @@ var search_vm = new Vue({
       if ((query.length === 0 || (as_prefix && query.length === 1)) && author.length === 0) {
         search_data.perms_search_results = null;
         search_data.perms_data_refreshed = null;
-        return null;
+      } else {
+        var perms_url = new URL('/api/v2/perms', window.location.href);
+        perms_url.searchParams.set('author', author);
+        perms_url.searchParams.set('module', query);
+        perms_url.searchParams.set('as_prefix', as_prefix ? 1 : 0);
+        fetch(perms_url).then(function(response) {
+          return response.json();
+        }).then(function(data) {
+          search_data.perms_search_results = data.data;
+          search_data.perms_data_refreshed = data.last_updated;
+        }).catch(function(error) {
+          console.log('Error retrieving perms', error);
+        });
       }
-      var perms_url = new URL('/api/v2/perms', window.location.href);
-      perms_url.searchParams.set('author', author);
-      perms_url.searchParams.set('module', query);
-      perms_url.searchParams.set('as_prefix', as_prefix ? 1 : 0);
-      fetch(perms_url).then(function(response) {
-        return response.json();
-      }).then(function(data) {
-        search_data.perms_search_results = data.data;
-        search_data.perms_data_refreshed = data.last_updated;
-      }).catch(function(error) {
-        console.log('Error retrieving perms', error);
-      });
     },
     search_authors: function() {
       var query = search_data.search_query;
@@ -83,18 +83,18 @@ var search_vm = new Vue({
       if (query.length === 0 || (as_prefix && query.length === 1)) {
         search_data.author_search_results = null;
         search_data.author_data_refreshed = null;
-        return null;
+      } else {
+        var authors_url = new URL('/api/v2/authors/' + encodeURIComponent(query), window.location.href);
+        authors_url.searchParams.set('as_prefix', as_prefix ? 1 : 0);
+        fetch(authors_url).then(function(response) {
+          return response.json();
+        }).then(function(data) {
+          search_data.author_search_results = data.data;
+          search_data.author_data_refreshed = data.last_updated;
+        }).catch(function(error) {
+          console.log('Error retrieving authors', error);
+        });
       }
-      var authors_url = new URL('/api/v2/authors/' + encodeURIComponent(query), window.location.href);
-      authors_url.searchParams.set('as_prefix', as_prefix ? 1 : 0);
-      fetch(authors_url).then(function(response) {
-        return response.json();
-      }).then(function(data) {
-        search_data.author_search_results = data.data;
-        search_data.author_data_refreshed = data.last_updated;
-      }).catch(function(error) {
-        console.log('Error retrieving authors', error);
-      });
     },
     module_url: function(module) {
       return 'https://metacpan.org/pod/' + encodeURI(module);
@@ -145,21 +145,28 @@ var search_vm = new Vue({
       }
     },
     to_date_string: function(epoch) {
-      if (epoch === null) { return ''; }
-      var date = new Date(epoch * 1000);
-      return date.toString();
+      if (epoch !== null) {
+        var date = new Date(epoch * 1000);
+        return date.toString();
+      } else {
+        return '';
+      }
     },
     to_utc_string: function(epoch) {
-      if (epoch === null) { return ''; }
-      var date = new Date(epoch * 1000);
-      return date.toUTCString();
+      if (epoch !== null) {
+        var date = new Date(epoch * 1000);
+        return date.toUTCString();
+      } else {
+        return '';
+      }
     },
     author_cpandir: function(author) {
       return author.substring(0, 1) + '/' + author.substring(0, 2) + '/' + author;
     },
     hash_from_search: function() {
       if (!search_data.changing_search) {
-        var new_hash = search_data.search_author + (search_data.search_match_mode === 'prefix' ? '~' : '=') + search_data.search_query;
+        var separator = search_data.search_match_mode === 'exact' ? '=' : '~';
+        var new_hash = search_data.search_author + separator + search_data.search_query;
         if ('#' + new_hash !== window.location.hash) {
           search_data.changing_hash = true;
           window.location.hash = new_hash;
@@ -170,18 +177,18 @@ var search_vm = new Vue({
       var hash = window.location.hash;
       if (hash != null && hash.length > 1 && hash.substring(0, 1) === '#') {
         var delim_index = hash.indexOf('=', 1);
-        if (delim_index === -1) {
+        if (delim_index !== -1) {
+          search_data.search_match_mode = 'exact';
+        } else {
           delim_index = hash.indexOf('~', 1);
-          if (delim_index === -1) {
-            return null;
-          } else {
+          if (delim_index !== -1) {
             search_data.search_match_mode = 'prefix';
           }
-        } else {
-          search_data.search_match_mode = 'exact';
         }
-        search_data.search_author = hash.substring(1, delim_index);
-        search_data.search_query = hash.substring(delim_index + 1);
+        if (delim_index !== -1) {
+          search_data.search_author = hash.substring(1, delim_index);
+          search_data.search_query = hash.substring(delim_index + 1);
+        }
       }
     }
   }
@@ -196,11 +203,12 @@ window.onload = function() {
 };
 
 window.addEventListener('hashchange', function() {
-  if (!search_data.changing_hash) {
+  if (search_data.changing_hash) {
+    search_data.changing_hash = false;
+  } else {
     search_vm.search_from_hash();
     search_data.changing_search = true;
     search_vm.do_search();
     search_data.changing_search = false;
   }
-  search_data.changing_hash = false;
 }, false);
